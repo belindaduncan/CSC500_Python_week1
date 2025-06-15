523c4c4466573277ec3b6a0b2c7bd61ee2c2e319
belindaduncan <142704660+belindaduncan@users.noreply.github.com>
2025-06-15 16:38:42 -0700

    Add files via upload
    Added week 1 Python Assignment. 
    The first week of Principles of Programming refreshed many foundational concepts related to programming. We reviewed the historical development of computers, the structure of algorithms, and applied these concepts by writing an application to perform simple functions such as addition, subtraction, multiplication, and division.

---
+++ Week 1 Creating Python Programs.py
@@ -0,0 +1,26 @@
+# Create a Python Programs
+# Part 1: 
+# Write a Python program to find the addition and subtraction of two numbers
+# User to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. Also, subtract the two numbers to find the output.
+
+# Read two integers from user input
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Perform and print the calculations
+print(num1 + num2)  # Addition
+print(num1 - num2)  # Subtraction
+
+# Part 2:
+# Find the multiplication and division of two numbers.
+
+# User to input two numbers (num3 and num4). 
+# multiply and divide num1/num2 to find the output.
+
+# Read two integers from user input
+num3 = int(input("Enter the Third integer: "))
+num4 = int(input("Enter the fourth integer: "))
+
+# Perform and print the calculations
+print(num3 * num4)  # Multiplication
+print(num3 / num4)  # Division
